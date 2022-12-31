5a9e27989af1133100e79fe87015429a1309a131
Dean <deaneffi1414@gmail.com>
2022-12-31 18:35:08 +0200

    List opens on project click, can add/remove todos

--- src/domCtrl.js
+++ src/domCtrl.js
@@ -1,9 +1,61 @@
-import {rmvProject,projects,formBtn,nameInp,dueInp,projectsCont,formCont,sections} from ".";
+import {rmvProject,projects,addProject,addTodo,rmvTodo} from ".";
+export {nameInp,dueInp,todoNameInp,displayTodos,openProject,}
 import doneSvg from "./check-bold.svg" 
 import trashSvg from "./trash-can.svg"
 
-let formShown = false
+//dom elements
+let projectFormBtn = document.querySelector(".project-form-btn")
+let todoFormBtn = document.querySelector(".todo-form-btn")
 
+let nameInp = document.querySelector("#name")
+let dueInp = document.querySelector("#due-date")
+
+let todoNameInp = document.querySelector("#todo-name")
+
+
+let projectsCont = document.querySelector(".projects-cont")
+let projectFormCont = document.querySelector(".project-form-cont")
+let TodoFormCont = document.querySelector(".todo-form-cont")
+
+
+
+let sections = document.querySelectorAll('.section')
+let projectsPlus = document.querySelector(".projects-plus")
+// let todosPlus = document.querySelector(".todos-plus")
+
+sections = Array.from(sections)
+let todoSection = document.querySelector(".todo-list")
+
+//listeners
+
+projectsPlus.addEventListener("click", function(){
+    formControl(projectFormCont)
+})
+
+projectFormBtn.addEventListener("click",function(){
+    addProject(projectFormCont)
+})
+
+
+// todosPlus.addEventListener("click", function(){
+//     formControl(TodoFormCont)
+// })
+
+todoFormBtn.addEventListener("click",function(){
+    addTodo(openProject,TodoFormCont)
+})
+
+
+
+
+// projectFormCont.style.display = "block"
+
+
+
+
+let openProject = 0
+
+let formShown = false
 export function displayProjects(){
     projectsCont.textContent = ''
     for(let i in projects){
@@ -26,7 +78,6 @@ export function displayProjects(){
         iconsDiv.append(done)
         iconsDiv.append(trash)
 
-
         if (projects[i].name == ''){
             projects[i].name = "Some Project"
         }
@@ -42,23 +93,31 @@ export function displayProjects(){
     }
 
 
-    let allTrash = document.querySelectorAll(".trash")
-    allTrash = Array.from(allTrash)
+    
+    let allTrash = Array.from(document.querySelectorAll(".trash"))
     for(let i = 0; i < allTrash.length; i++){
-        allTrash[i].addEventListener("click",function(){rmvProject(i)})
+        allTrash[i].addEventListener("click",function(){
+            rmvProject(i)
+        })
+    }
+
+
+    let allProject = Array.from(document.querySelectorAll(".project"))
+    for(let i = 0; i < allProject.length; i++){
+        allProject[i].addEventListener("click",function(){displayTodos(i)})
     }
 
 }
 
-export function formControl(){
+export function formControl(form){
     if(formShown){
-        formCont.style.display = "none"
+        form.style.display = "none"
         formShown = false
         blurOnForm()
 
 
     }else{
-        formCont.style.display = "block"
+        form.style.display = "block"
         formShown = true
         blurOnForm()
 
@@ -79,4 +138,62 @@ function blurOnForm(){
    
 }
 
+
+function displayTodos(i){
+    openProject = i
+    todoSection.textContent = ''
+    let todosTitle = document.createElement("div")
+    let projectTitle = document.createElement("h1")
+    let todosPlus = document.createElement("span")
+
+    todosTitle.classList.add("my-todos-title")
+    todosPlus.textContent = "+"
+    todosPlus.classList.add("plus","todos-plus")
+
+
+
+    projectTitle.textContent = projects[i].name
+    todoSection.append(todosTitle)
+    todosTitle.append(projectTitle)
+    todosTitle.append(todosPlus)
+
+
+    todosPlus.addEventListener("click", function(){
+            formControl(TodoFormCont)
+    })
+
+    let todosUl = document.createElement("ul")
+    todosUl.classList.add("todos-ul")
+    todoSection.append(todosUl)
+
+    for(let t in projects[i].todos){
+        let todoItem = document.createElement("li")
+        let icons = document.createElement("div")
+        let done = new Image()
+        let trash = new Image()
+        done.src = doneSvg
+        trash.src = trashSvg
+        
+        todoItem.classList.add("todo-item")
+        todoItem.textContent = projects[i].todos[t]
+        todosUl.append(todoItem)
+        
+        icons.classList.add("icons")
+        done.classList.add("icon")
+        trash.classList.add("icon","todo-trash")
+
+        icons.append(done,trash)
+        todoItem.prepend(icons)
+        
+    }
+
+    let allIconTrash = Array.from(document.querySelectorAll(".todo-trash"))
+    
+    for(let i in allIconTrash){
+        allIconTrash[i].addEventListener("click",rmvTodo)
+    }
+
+
+
+}
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import {displayProjects,formControl} from "./domCtrl.js"
-export {projects,formBtn,nameInp,dueInp,projectsCont,formCont,sections,rmvProject}
+import {displayProjects,formControl,nameInp,dueInp, todoNameInp,displayTodos,openProject} from "./domCtrl.js"
+export {projects,rmvProject,addProject,addTodo,rmvTodo}
 
 //array to hold all project objects and use it to display them on dom 
 const projects = []
@@ -16,11 +16,7 @@ class Project{
         projects.push(this)
     }
 
-    removeProject(){
-        projects.splice(projects.indexOf(this),projects.indexOf(this)+1)
-    }
-
-    addTodo(todo){
+    pushTodo(todo){
         this.todos.push(todo)
     }
 }
@@ -32,41 +28,39 @@ class Todo{
     }
 }
 
-
-function addProject(){
+function addProject(form){
     projects.push(new Project(nameInp.value,dueInp.value,3))
+    formControl(form)
     displayProjects()
-    formControl()
 }
-
 
 function rmvProject(i){
     projects.splice(i,1)
     displayProjects()
+}
+
+function addTodo(i, form){
+    projects[i].todos.push(todoNameInp.value)
+    console.log(projects);
+    displayTodos(i)
+    console.log(form);
+    formControl(form)
 
 }
-//dom elements
-let formBtn = document.querySelector(".form-btn")
-let nameInp = document.querySelector("#name")
-let dueInp = document.querySelector("#due-date")
-let projectsCont = document.querySelector(".projects-cont")
-let formCont = document.querySelector(".form-cont")
-let sections = document.querySelectorAll('.section')
-let projectsPlus = document.querySelector(".projects-plus")
-sections = Array.from(sections)
 
-//listeners
-formBtn.addEventListener("click",addProject)
-projectsPlus.addEventListener("click", formControl)
+function rmvTodo(i){
+    projects[openProject].todos.splice(i,1)
+    displayTodos(openProject)
 
-let release = new Project("son of god","22.3.2023",3,false)
-release.addTodo(new Todo("adpot","adpot a pet"))
+}
 
+let release = new Project("Armdilo to garbadge","22.3.2023",3,false)
+release.pushTodo("adpot a pet")
+release.pushTodo("fuck a duck")
 
+release.pushProject()
+release.pushProject()
+console.log(release);
 
-addProject(release)
-addProject(release)
-
-
-
-console.log(projects);
+displayProjects()
+displayTodos(openProject)
